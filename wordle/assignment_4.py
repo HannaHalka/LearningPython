c8ad2a11a5fbf9e7c41a4262689de7cbaa0e6c44
Hanna <hanna.dm.galka@gmail.com>
2023-10-29 19:53:24 +0200

    added hex, added mutual conversions

--- wordle/assignment_4.py
+++ wordle/assignment_4.py
@@ -56,12 +56,91 @@ def prompt_for_bool(prompt: str) -> bool:
             return False
 
 
+def hex_to_decimal(input_value: str) -> int:
+    assert input_value[:2:] == '0x'
+    input_value = input_value[2::]
+    result = 0
+    rank = 0
+    for digit in input_value[::-1]:
+        result += (16 ** rank) * int(digit, 16)
+        rank += 1
+    return result
+
+
+assert hex_to_decimal('0x0') == 0
+assert hex_to_decimal('0x1') == 1
+assert hex_to_decimal('0xAA') == 170
+assert hex_to_decimal('0xFF') == 255
+assert hex_to_decimal('0xff') == 255
+
+
+def decimal_to_hex_digit(input_value: int) -> str:
+    if input_value == 10: return 'A'
+    if input_value == 11: return 'B'
+    if input_value == 12: return 'C'
+    if input_value == 13: return 'D'
+    if input_value == 14: return 'E'
+    if input_value == 15: return 'F'
+    return str(input_value)
+
+
+def decimal_to_hex(input_value: int) -> str:
+    assert input_value >= 0
+    if input_value == 0:
+        return '0x0'
+    result = ''
+    while input_value > 0:
+        remainder = input_value % 16
+        hex_digit = decimal_to_hex_digit(remainder)
+        result = hex_digit + result
+        input_value = input_value // 16
+    return '0x' + result
+
+
+assert decimal_to_hex(0) == '0x0'
+assert decimal_to_hex(1) == '0x1'
+assert decimal_to_hex(16) == '0x10'
+assert decimal_to_hex(170) == '0xAA'
+
+
+def get_number_system():
+    while True:
+        inp = input("To which number system to convert? \n1 - binary \n2 - decimal \n3 - hex\n  ->  ")
+        if inp == '1': return 2
+        if inp == '2': return 10
+        if inp == '3': return 16
+
+
 while True:
     user_input = input('Please enter a number: ')
+
+    base = get_number_system()
+
     if user_input[:2:] == '0b':
-        print(binary_to_decimal(user_input))
+        if base == 2:
+            print(user_input)
+        elif base == 10:
+            print(binary_to_decimal(user_input))
+        else:
+            dec = binary_to_decimal(user_input)
+            print(decimal_to_hex(dec))
+
+    elif user_input[:2:] == '0x':
+        if base == 2:
+            dec = hex_to_decimal(user_input)
+            print(decimal_to_binary(dec))
+        elif base == 10:
+            print(hex_to_decimal(user_input))
+        else:
+            print(user_input)
+
     else:
-        print(decimal_to_binary(int(user_input)))
+        if base == 2:
+            print(decimal_to_binary(int(user_input)))
+        elif base == 10:
+            print(user_input)
+        else:
+            print(decimal_to_hex(int(user_input)))
 
     if not prompt_for_bool('Continue? '):
         break
